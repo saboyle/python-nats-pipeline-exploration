2505071504a382134dc2e38b6cf0fcd78c99bb69
Steve Boyle <boylesa@gmail.com>
2019-01-10 00:41:32 +0000

    Closes #4: Added basic Aggregator, 1st draft.

--- utils/README.md
+++ utils/README.md
@@ -14,3 +14,11 @@ chaining using standard unix pipes / filters etc if needed.
 1. python nats-wiretap.py p1.s1 - Listen to Pipeline P1, Stage s1.
 2. python nats-wiretap.py p1.*  - Listen to Pipleine P1 across all stages.
 
+## nats-aggregator [Ref: EIP - Hohpe & Woolfe, 268]
+
+``` bash
+python nats-aggregator.py [queue-spec]
+```
+Example of stateful aggregator.  State is held in process as a dict, 
+checked on receipt of each message with an aggregated summary published to a new queue 
+when each of the constituent messages have been received.
---
+++ utils/nats-aggregator.py
@@ -0,0 +1,74 @@
+import sys
+import asyncio
+import time
+import signal
+import json
+
+from nats.aio.client import Client as NATS
+
+
+cache = {}
+
+
+def aggregator(loop, subject):
+    #####################
+    # Connection to NATS
+    #####################
+    nc = NATS()
+    yield from nc.connect("localhost:4222", loop=loop)
+
+    ################################################################################
+    # Aggregate stage timestamps into a single dict and publish summary to new queue
+    ################################################################################
+    async def aggregate(msg):
+        jmsg = json.loads(msg.data.decode())
+        subject = msg.subject
+
+        if subject == "p1.s0":
+            pass
+        else:
+            correlation_id = jmsg['correlation_id']
+
+            ts = time.time()
+
+            if correlation_id in cache:
+                # print("Correlation Id in cache")
+
+                if subject in cache[correlation_id]:
+                    print('Error - loop detected')
+                else:
+                    cache[correlation_id][subject] = ts
+
+                    # Check if all stages in the pipeline are complete and publish to a control queue.
+                    if sorted(cache[correlation_id].keys()) == ['p1.s1', 'p1.s2', 'p1.s3']:
+                        await nc.publish('c1.monitor', json.dumps(cache[correlation_id]).encode('utf-8'))
+                        print("Completed: ", correlation_id, cache[correlation_id])
+
+            else:
+                # print("NEW Correlation Id")
+                cache[correlation_id] = {}
+                cache[correlation_id][subject] = ts
+                # print(cache)
+
+    #####################
+    # Message Handlers
+    #####################
+    async def mh_s1(msg):
+        await aggregate(msg)
+
+    yield from nc.subscribe(subject, cb=mh_s1)
+
+
+def signal_handler(sig, frame):
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(aggregator(event_loop, subject="p1.*"))
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print('Aggregator enabled: Listening to ', "p1.*")
+    event_loop.run_forever()
